48e34643c5f0ac3290bb7f65d43691eecdcc3897
NHOY <NHOY@2210DR000919>
2023-06-09 19:07:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp58/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commandCloseProgram = "exit";
+            string userInput;
+            bool isUserExit = false;
+
+            while (isUserExit == false)
+            {
+                Console.WriteLine("Введите команду - " + "\"" + commandCloseProgram + "\"" + " для закрытия программы.");
+                userInput = Console.ReadLine();
+
+                if (userInput == commandCloseProgram)
+                {
+                    isUserExit = true;
+                }
+            }
+        }
+    }
+}
